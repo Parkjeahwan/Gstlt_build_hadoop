4d8ab2a1fd17a7e5e85ec3dfdae9883ce2ae7d69
parkjh <parkluck@clunix.com>
2016-01-21 10:28:19 +0900

    mrunit test code add

---
+++ FasTopValue/src/main/java/com/clunix/Gstlt_utilsTest.java
@@ -0,0 +1,23 @@
+package com.clunix;
+
+import static org.junit.Assert.*;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Test;
+
+public class Gstlt_utilsTest
+{
+	MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
+	ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver;
+	MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+}
